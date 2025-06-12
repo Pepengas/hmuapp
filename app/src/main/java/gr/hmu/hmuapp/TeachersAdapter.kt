dc37919d26cfc44b154d0cce3cab98dee69527e0
Pepengas <96621785+Pepengas@users.noreply.github.com>
2025-06-12 22:42:28 +0300

    Extend teacher info

--- app/src/main/java/gr/hmu/hmuapp/TeachersAdapter.kt
+++ app/src/main/java/gr/hmu/hmuapp/TeachersAdapter.kt
@@ -35,11 +35,17 @@ class TeachersAdapter(
         private val name: TextView = view.findViewById(R.id.name)
         private val title: TextView = view.findViewById(R.id.title)
         private val department: TextView = view.findViewById(R.id.department)
+        private val phone: TextView = view.findViewById(R.id.phone)
+        private val email: TextView = view.findViewById(R.id.email)
+        private val interests: TextView = view.findViewById(R.id.interests)
 
         fun bind(item: Teacher) {
             name.text = item.name
             title.text = item.title
             department.text = item.department
+            phone.text = item.phone
+            email.text = item.email
+            interests.text = item.interests
             itemView.setOnClickListener { onItemClick(item) }
         }
     }
