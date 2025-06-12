687a4d69e5f89502ea3762f99608c9cffb7cadad
Pepengas <96621785+Pepengas@users.noreply.github.com>
2025-06-12 10:31:14 +0300

    Remove binary gradle wrapper

---
+++ app/src/main/java/gr/hmu/hmuapp/EventsFragment.kt
@@ -0,0 +1,5 @@
+package gr.hmu.hmuapp
+
+import androidx.fragment.app.Fragment
+
+class EventsFragment : Fragment(R.layout.fragment_events)
---
+++ app/src/main/java/gr/hmu/hmuapp/TeachersFragment.kt
@@ -0,0 +1,5 @@
+package gr.hmu.hmuapp
+
+import androidx.fragment.app.Fragment
+
+class TeachersFragment : Fragment(R.layout.fragment_teachers)
