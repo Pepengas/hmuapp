6bdbba647d41aa9e3469c0914effec8f3841a6c1
Pepengas <96621785+Pepengas@users.noreply.github.com>
2025-06-12 23:02:25 +0300

    Rename helper to avoid compile conflicts

--- app/src/main/java/gr/hmu/hmuapp/data/Teacher.kt
+++ app/src/main/java/gr/hmu/hmuapp/data/Teacher.kt
@@ -4,5 +4,8 @@ data class Teacher(
     val name: String,
     val title: String,
     val department: String,
+    val phone: String,
+    val email: String,
+    val interests: String,
     val profileUrl: String
 )
